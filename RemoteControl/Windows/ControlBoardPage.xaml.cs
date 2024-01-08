137e2a53abe4b8515405b79a06a8862d883570ba
Somanahali Keshavamurthy, Darshan <dsomanah@itron.com>
2024-01-08 14:55:00 +0530

    Commit after final changes to the project

--- RemoteControl/Windows/ControlBoardPage.xaml.cs
+++ RemoteControl/Windows/ControlBoardPage.xaml.cs
@@ -9,22 +9,71 @@ public partial class ControlBoardPage : ContentPage
 
     private void RemoteControlM1TurnOnBtnClicked(object sender, EventArgs e)
     {
-        RemoteControlM1Status.Text = "ON";
+        if (RemoteControlM1Status.Text == "OFF")
+        {
+            RemoteControlM1Status.Text = "ON";
+            UpdatedAttemptsCount();
+        }
+    }
+
+    private void UpdatedAttemptsCount()
+    {
+        int currentCount = Convert.ToInt32(TotalNoOfAttemps.Text);
+        int attemptsCount = currentCount + 1;
+        TotalNoOfAttemps.Text = attemptsCount.ToString();
+
+        if (TotalNoOfAttempsPending.Text == "1")
+        {
+            TotalNoOfAttempsPending.Text = "50";
+        }
+        int currentBalanceCount = Convert.ToInt32(TotalNoOfAttempsPending.Text);
+        int attemptsBalanceCount = currentBalanceCount - 1;
+        TotalNoOfAttempsPending.Text = attemptsBalanceCount.ToString();
     }
 
     private void RemoteControlM1TurnOffBtnClicked(object sender, EventArgs e)
     {
-        RemoteControlM1Status.Text = "OFF";
+        if (RemoteControlM1Status.Text == "ON")
+        {
+            RemoteControlM1Status.Text = "OFF";
+            UpdatedAttemptsCount();
+        }
     }
 
     private void RemoteControlM2TurnOnBtnClicked(object sender, EventArgs e)
     {
-        RemoteControlM2Status.Text = "ON";
+        if (RemoteControlM2Status.Text == "OFF")
+        {
+            RemoteControlM2Status.Text = "ON";
+            UpdatedAttemptsCount();
+        }
     }
 
     private void RemoteControlM2TurnOffBtnClicked(object sender, EventArgs e)
     {
-        RemoteControlM2Status.Text = "OFF";
+        if (RemoteControlM2Status.Text == "ON")
+        {
+            RemoteControlM2Status.Text = "OFF";
+            UpdatedAttemptsCount();
+        }
+    }
+
+    private void RemoteControlM3TurnOnBtnClicked(object sender, EventArgs e)
+    {
+        if (RemoteControlM3Status.Text == "OFF")
+        {
+            RemoteControlM3Status.Text = "ON";
+            UpdatedAttemptsCount();
+        }
+    }
+
+    private void RemoteControlM3TurnOffBtnClicked(object sender, EventArgs e)
+    {
+        if (RemoteControlM3Status.Text == "ON")
+        {
+            RemoteControlM3Status.Text = "OFF";
+            UpdatedAttemptsCount();
+        }
     }
 
     private void OnLogoutClicked(object sender, EventArgs e)
